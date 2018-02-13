5e5daf215b9f6a30f4a71f008f14553895268c73
Swastik Biswas <swastikbiswas2016@gmail.com>
2018-02-13 13:12:32 +0530

    updated for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var server_port = process.env.PORT || 8080;
-//app.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 //Routes
 //home page route

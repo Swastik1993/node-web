cfb2cd1893e30de36747d549b7d863f19d1e68d5
Swastik Biswas <swastikbiswas2016@gmail.com>
2018-03-11 18:05:31 +0530

    re testing

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: 'sbiswas1993@gmail.com',
   from: 'mail@swastikbiswas.me',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  subject: 'Testing mail from SendGrid WebAPI',
+  text: 'Sending mails is easy to do anywhere, even with Node.js',
+  html: '<strong>Mail is sent with Node.js as backend</strong>',
 };
 sgMail.send(msg);
 

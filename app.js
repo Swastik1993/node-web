5be952818876b0b1ea92e10b5b3b0a688ad428d2
Swastik Biswas <swastikbiswas2016@gmail.com>
2018-03-15 23:22:38 +0530

    updated index page

--- app.js
+++ app.js
@@ -26,17 +26,3 @@ app.listen(server_port, function() {
     console.log("Application running on http://localhost:"+server_port);
 });
 
-
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'swastikbiswas2016@gmail.com',
-  from: 'mail@swastikbiswas.me',
-  subject: 'Testing mail from SendGrid WebAPI',
-  text: 'Sending mails is easy to do anywhere, even with Node.js',
-  html: '<strong>Mail is sent with Node.js as backend</strong>',
-};
-sgMail.send(msg);
-
